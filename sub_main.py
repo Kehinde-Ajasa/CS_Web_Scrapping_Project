655eaf0d579a3f7de14266a86589635091a669bb
Kehinde Ajasa <ti664469@gmail.com>
2023-10-12 22:30:03 +0100

    First commmit, sub file created.

--- sub_main.py
+++ sub_main.py
@@ -1,9 +1,10 @@
-from bs4 import BeautifulSoup
+class webscrapping:
+	from bs4 import BeautifulSoup
 
 
-with open("webscrapper.html", "r") as myfile:
-	content = myfile.read()
+	with open("webscrapper.html", "r") as myfile:
+		content = myfile.read()
 
-	soup = BeautifulSoup(content, "lxml")
-	tags = soup.find_all('head')
-	print(tags)
+		soup = BeautifulSoup(content, "lxml")
+		tags = soup.find_all('head')
+		print(tags)
