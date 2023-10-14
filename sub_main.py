7942089b747d067b14074195617810c37d990eb5
Kehinde Ajasa <ti664469@gmail.com>
2023-10-14 15:46:36 +0100

    First Test on Wikipedia

--- sub_main.py
+++ sub_main.py
@@ -1,16 +1,26 @@
-class webscrapping:
-	from bs4 import BeautifulSoup
+import requests
+from bs4 import BeautifulSoup
+import wikipediaapi
 
+def search_wikipedia(query):
+    # Create a Wikipedia API object
+    wiki_wiki = wikipediaapi.Wikipedia('en')
 
-	with open("WEBSCRAPPER.html", "r") as myfile:
-		content = myfile.read()
+    # Search for the given query
+    page = wiki_wiki.page(query)
 
-		soup = BeautifulSoup(content, "lxml")
-		course_template = soup.find_all("div", class_="course")
-		for courses in course_template:
-			course_name = courses.h2.text
-			course_price = courses.find_all("p")[-1].text
-			course_description = courses.find_all("p")[0].text
-			print(course_name)
-			print(f'Description: {course_description}')
-			print(course_price)
+    if page.exists():
+        # Get the Wikipedia page title and content
+        page_title = page.title
+        page_content = page.text
+
+        # Print the title and a summary
+        print("Title:", page_title)
+        print("Summary:")
+        print(page_content[:500])  # Print the first 500 characters as a summary
+    else:
+        print("Page not found on Wikipedia.")
+
+if __name__ == "__main__":
+    search_query = input("Enter a word to search on Wikipedia: ")
+    search_wikipedia(search_query)
