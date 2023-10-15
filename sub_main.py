d37135f4c4303c5e7f0ab7f7f150c715ebcf02bc
Kehinde Ajasa <ti664469@gmail.com>
2023-10-15 10:18:49 +0100

    Option to select language was added

--- sub_main.py
+++ sub_main.py
@@ -10,17 +10,18 @@ class SummarizeVersion:
 
     def get_summary(self):
         try:
-            # Set the language for Wikipedia (default is 'en' for English)
-            wikipedia.set_lang("en")
+            # Set the language for Wikipedia
+            user_language = input("Which language would you like to learn in: ").lower()
+            wikipedia.set_lang(user_language[:2])
 
             # Search for the given query
             search_results = wikipedia.search(self.query)
 
             if search_results:
                 for index, context in enumerate(search_results):
-                    print(f"Summary {index + 1}: {context}")
+                    print(f"Topic {index + 1}: {context}")
 
-                display_page = int(input("Select Page: "))
+                display_page = int(input("Select Topic to learn: "))
                 # Get the user's page result
                 result_page = wikipedia.page(search_results[display_page - 1])
                 # Print the title and a summary
@@ -37,6 +38,6 @@ class SummarizeVersion:
 
 
 if __name__ == "__main__":
-    search_query = input("What do you want to search for:  ")
+    search_query = input("What do you want to learn about:  ")
     search_instance = SummarizeVersion(search_query)
     search_instance.get_summary()
